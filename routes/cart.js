60f2997c0fd4af85869f7802e82cda16bdf94b82
akshay <akshay123gangishetty@gmail.com>
2022-05-27 13:26:37 +0530

    E-Commerce API

---
+++ routes/cart.js
@@ -0,0 +1,57 @@
+const router = require("express").Router();
+const { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("../middleware/verifyToken");
+const Cart = require("../models/Cart");
+
+router.post("/", verifyTokenAndAuthorization, async (req, res) => {
+    const newCart = new Cart(req.body);
+    try {
+        const savedCart = await newCart.save();
+        res.status(200).json(savedCart);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        const updatedCart = await Cart.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+        res.status(200).json(updatedCart);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await Cart.findByIdAndDelete(req.params.id);
+        res.status(200).json("Deleted");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        const cart = await Cart.findOne({ userId: req.params.userId });
+        res.status(200).json(cart);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const carts = await Cart.find();
+        res.status(200).json(carts);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
